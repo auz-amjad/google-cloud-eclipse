4e27189cd799650e1ccc410d62ebdd52b4e24b10
Brian de Alwis <bsd@mt.ca>
2017-07-20 13:43:01 -0400

    Install Google Cloud SDK into location with spaces

--- build/install-cloudsdk.sh
+++ build/install-cloudsdk.sh
@@ -12,15 +12,15 @@ fi
 # ensure we error out on failures and unset variables
 set -o errexit -o nounset
 
-CLOUDSDK_VERSION=$1
-INSTALLDIR=$2
+CLOUDSDK_VERSION="$1"
+INSTALLDIR="$2"
 
-CLOUDSDKDIR=${INSTALLDIR}/google-cloud-sdk
+CLOUDSDKDIR="${INSTALLDIR}/google-cloud-sdk"
 
-currentVersion=$(if [ -f ${CLOUDSDKDIR}/VERSION ]; then cat ${CLOUDSDKDIR}/VERSION; fi)
+currentVersion=$(if [ -f "${CLOUDSDKDIR}"/VERSION ]; then cat "${CLOUDSDKDIR}"/VERSION; fi)
 
 if [ "${currentVersion}" = "${CLOUDSDK_VERSION}" ]; then
-    echo "Google Cloud SDK at version $(cat ${CLOUDSDKDIR}/VERSION)"
+    echo "Google Cloud SDK at version $(cat "${CLOUDSDKDIR}"/VERSION)"
     exit 0
 elif [ -n "${currentVersion}" ]; then
     echo "Replacing found Google Cloud SDK version ${currentVersion}"
@@ -34,23 +34,23 @@ die() {
     else
         echo "$0: ERROR" 1>&2
     fi
-    rm -rf ${CLOUDSDKDIR}
+    rm -rf "${CLOUDSDKDIR}"
     exit 1
 }
 
-rm -rf ${CLOUDSDKDIR}
+rm -rf "${CLOUDSDKDIR}"
 
 echo ">> download google-cloud-sdk-${CLOUDSDK_VERSION}"
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${CLOUDSDK_VERSION}-linux-x86_64.tar.gz || die
-tar -xzf google-cloud-sdk-${CLOUDSDK_VERSION}-linux-x86_64.tar.gz -C ${INSTALLDIR} || die
+tar -xzf google-cloud-sdk-${CLOUDSDK_VERSION}-linux-x86_64.tar.gz -C "${INSTALLDIR}" || die
 
 ## DISABLED: Updating to latest version should be done outside
 ## update all Cloud SDK components
 # echo ">> gcloud components update"
-# ${CLOUDSDKDIR}/bin/gcloud components update --quiet || die
+# "${CLOUDSDKDIR}"/bin/gcloud components update --quiet || die
 
 # add App Engine component to Cloud SDK
 echo ">> gcloud components install app-engine-java"
-${CLOUDSDKDIR}/bin/gcloud components install app-engine-java --quiet || die
+"${CLOUDSDKDIR}"/bin/gcloud components install app-engine-java --quiet || die
 
 
